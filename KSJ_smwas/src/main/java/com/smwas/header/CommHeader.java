4d9006f0a61a8186943c52ac08bb6be676ca784d
ksj <tmdwnchlrh3@gmail.com>
2024-02-26 09:12:13 +0900

    IP 주소변경

--- KSJ_smwas/src/main/java/com/smwas/header/CommHeader.java
+++ KSJ_smwas/src/main/java/com/smwas/header/CommHeader.java
@@ -10,13 +10,13 @@ public class CommHeader {
 	 * 헤더
 	 */
 	// private static String authorization = "Bearer "; // (+ 접근토큰) String(40), 접근토큰
-	// (발급받은 접근토큰 앞에 "Bearer" 붙여서 호출)
+	// (한투에서 발급받은 접근토큰 앞에 "Bearer" 붙여서 호출)
 	private static String authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjM1MjMzNDE0LTkxMGQtNDk0Yy05ZGI1LWFiYTZlNDI5M2JjNiIsImlzcyI6InVub2d3IiwiZXhwIjoxNzA4Mzg0NzgwLCJpYXQiOjE3MDgyOTgzODAsImp0aSI6IlBTVURvNGRaSGpES1NJNTdOUzRmYjJ5T25ESlV3UXk3SGYxNSJ9.zVC9ee6D2WXJWa8wufxH4OANo1uaM3mrxOOQzGdRSIN_itTRKAZOJGnrK-J3bkRyw_JWuL5DwRSZzd5-0uV0rA"
 			+ "";
 
-//	private static String appkey = "PSUDo4dZHjDKSI57NS4fb2yOnDJUwQy7Hf15"; // APP KEY
+//	private static String appkey = "PSUDo4dZHjDKSI57NS4fb2yOnDJUwQy7Hf15"; // 한투 APP KEY
 //	private static String appsecret = "0Oi2jJuwLG3jsx+5qHVVI73YX9YEQKNkX5Q8X2S/Pg8wtVYdsXp0pYtwQqX/u51WOnIKJwLfF+00Yi44SVybqd9Ks+V0zwTl6K7rxszNh3TCAFQGJpHR+c9NJq5DhaGaKC3OLgcrLIHvlrXcWNEbZdoPIefBGmtxTmgAf3DW490MajtSuKY=";
-	private static String appkey = "PSJeY9chYB8UgdUt9Me8LIMsdexaO6XKs1EK"; // APP KEY
+	private static String appkey = "PSJeY9chYB8UgdUt9Me8LIMsdexaO6XKs1EK"; // 한투 APP KEY
 	private static String appsecret = "Iiuock65BYo4WLDGlTb1ca9fNfOOlocaNwyAERNyO9+ZAwB4mEDDeIaCeOigAlI1J5l7OU/r52AaW3ZxcYbbDY1rZWpYQMKEddnzggH4F8x7g9V+rlhkNkmGHv7Uz+Mank0dh2qkGYaG0zye8skixXyEwMsjEFzeeLAD0RRVHPGc+crEOKg=";
 	private static String custtype = "P"; // String(1) 고객타입 B:법인, P:개인
 	private static String grantType = "client_credentials"; // String(18) 권한부여타입 -- 토큰 및 실시간 접속키 발급시 사용
